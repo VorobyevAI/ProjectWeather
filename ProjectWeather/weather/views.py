2782038b6f055bc21d9231071bb083cb9dda9bbf
VorobyevAI <andrey135676@mail.ru>
2022-08-14 09:52:22 +0500

    finish the design, and add functionality for deleting and time by city

--- ProjectWeather/weather/views.py
+++ ProjectWeather/weather/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render
 from requests import request, get
 from datetime import datetime
+from .models import City
+from .forms import CityForm
 
 def index(request):
     appid = 'd74f42ecfaf3dfd799b232e7020f48e9'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
-    city = 'Moscow'
-    res = get(url.format(city)).json()
-    time_now = datetime.now()
-    city_info = {
-        'city': city,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon'],
-        'description': res['weather'][0]['description'],
-        'time': time_now.time()
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
-    }
-    print(res)
-    contex = {'info': city_info}
+
+    form = CityForm()
+
+    cities = City.objects.all()
+    time_now = datetime.now()
+    all_cities = []
+    for city in cities:
+        res = get(url.format(city.name)).json()
+
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon'],
+            'description': res['weather'][0]['description'],
+            'time': time_now.time()
+
+        }
+        all_cities.append(city_info)
+
+    contex = {'info': all_cities, 'form': form}
     return render(request, 'weather/index.html', contex)
